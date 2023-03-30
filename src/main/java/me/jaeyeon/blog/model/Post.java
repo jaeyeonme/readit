44c623a52baad24b39a9da8f367097f297c39cd6
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-03-30 16:34:08 +0900

    fix : BaseTimeEntity 추가 및 Domain 리팩토링 및 추가

--- src/main/java/me/jaeyeon/blog/model/Post.java
+++ src/main/java/me/jaeyeon/blog/model/Post.java
@@ -1,22 +1,32 @@
 package me.jaeyeon.blog.model;
 
-import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Getter @Setter
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post {
+public class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "title", nullable = false, unique = true)
+    @Column(name = "title", nullable = false)
     private String title;
 
     @Column(name = "description", nullable = false)
@@ -25,6 +35,19 @@ public class Post {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member author;
+
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Comment> comments = new HashSet<>();
+    private List<Comment> comments = new ArrayList<>();
+
+    public void setAuthor(Member member) {
+        if (this.author != null) {
+            this.author.getPosts().remove(this);
+        }
+
+        this.author = member;
+        member.getPosts().add(this);
+    }
 }
---
+++ src/main/java/me/jaeyeon/blog/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package me.jaeyeon.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import me.jaeyeon.blog.model.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
