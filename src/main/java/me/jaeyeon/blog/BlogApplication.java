cb86f943a5e02a9469dd14ee960fa37e100dceb3
Jaeyeon <59726665+jaeyeonme@users.noreply.github.com>
2023-03-30 16:46:30 +0900

    Revert "fix : BaseTimeEntity 추가 및 Domain 리팩토링 및 추가"

--- src/main/java/me/jaeyeon/blog/BlogApplication.java
+++ src/main/java/me/jaeyeon/blog/BlogApplication.java
@@ -2,9 +2,7 @@ package me.jaeyeon.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
 
--- src/main/java/me/jaeyeon/blog/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package me.jaeyeon.blog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.jaeyeon.blog.model.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
