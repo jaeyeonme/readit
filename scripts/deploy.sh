07033df8d57f322c2ad50c1649897877e8c77e3b
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-06-14 12:34:22 +0900

    fix : fix deploy setings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-cd /home/ec2-user/app
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(sudo docker container ls -q)
 
-DOCKER_APP_NAME=spring
-
-# 실행중인 blue가 있는지
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
-
-# green이 실행중이면 blue up
-if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-
-	sleep 30
-
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-	docker image prune -af # 사용하지 않는 이미지 삭제
-
-# blue가 실행중이면 green up
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
-	echo "green up"
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
-
-	sleep 30
-
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-	docker image prune -af
+  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+  sudo docker stop $CURRENT_PID
+  sleep 5
 fi
+
+cd /home/ec2-user/app
+sudo docker build -t reddit-api-spring-boot-docker .
+sudo docker run -d -p 8080:8080 reddit-api-spring-boot-docker
