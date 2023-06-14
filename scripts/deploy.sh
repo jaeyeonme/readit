d56e778dd76fa3d8e1476182bacce3db21425bdc
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-06-14 16:35:48 +0900

    add : add docker-compose files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
-CURRENT_PID=$(sudo docker container ls -q)
+DOCKER_APP_NAME=readit
+IMAGE_NAME=ghcr.io/jaeyeonme/readit:latest
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+# Docker 이미지 pull
+sudo docker pull $IMAGE_NAME
+
+# 실행중인 blue가 있는지
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+# green이 실행중이면 blue up
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue up"
+    sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+    sleep 30
+
+    sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+    sudo docker image prune -af # 사용하지 않는 이미지 삭제
+
+# blue가 실행중이면 green up
 else
-  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
-  sudo docker stop $CURRENT_PID
-  sleep 5
+    echo "green up"
+    sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+
+    sleep 30
+
+    sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+    sudo docker image prune -af
 fi
-
-echo "> Docker image pull from Github Container Registry" >> /home/ec2-user/deploy.log
-sudo docker pull ghcr.io/jaeyeonme/readit:latest
-
-echo "> JAR file download from S3" >> /home/ec2-user/deploy.log
-aws s3 cp s3://reddit-s3/blog-0.0.1-SNAPSHOT.jar /home/ec2-user/app/
-
-cd /home/ec2-user/app
-sudo docker run -d -p 8080:8080 -v $(pwd):/app ghcr.io/jaeyeonme/readit:latest
